9eb5da0ae5f2891dfe7d311f1486c2ad7d1e11b1
jaroslaw madrala <jaroslaw.madrala@gmail.com>
2019-06-14 20:57:00 +0200

    fixed untracked files

---
+++ WebUserRole/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebUserRole.Data;
+
+[assembly: HostingStartup(typeof(WebUserRole.Areas.Identity.IdentityHostingStartup))]
+namespace WebUserRole.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
